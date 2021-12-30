dad522ee6dd3478caeb2eb065aa2474046703ae7
gowtham <kasigowtham7@gmail.com>
2021-12-30 15:29:25 +0530

    getCustomer parameter updated

--- controllers/checkoutController.js
+++ controllers/checkoutController.js
@@ -1,59 +1,25 @@
 const catchAsync = require("../utils/catchAsync");
-const stripeController = require("../utils/stripe");
+const stripeService = require("../utils/stripe");
 const { handleResponse, handleError } = require("../utils/responseHandler");
-const {
-  default: strictTransportSecurity,
-} = require("helmet/dist/middlewares/strict-transport-security");
-
-const stripe = require("stripe")(
-  "sk_test_51JPkNQSGmAfV4DJDikEEAE6waE7NXPImUNY4zSKB5ZxsAywOczXQXJRuF2tR5BtyIQ7oqw4bxbH81JdatlVjDytR00hSEI9CG5"
-);
 
 exports.checkoutSession = catchAsync(async (req, res) => {
-  const { customerId, cardDetails } = req.body;
-
-  //   //checkout using stripe payment
-  const card = await stripeController.addCardToCustomer(
-    cardDetails,
-    customerId
-  );
-  if (card) {
-    const paymentIntent = await stripeController.createPaymentIntent(
-      customerId
+  //checkout using stripe payment
+  const { default_source } = await stripeService.getCustomer(req.user.stripeId);
+  if (default_source) {
+    const paymentIntent = await stripeService.createPaymentIntent(
+      req.user.stripeId
     );
-    if (paymentIntent) {
-      const payment = await stripeController.confirmPayment(
-        paymentIntent.id,
-        card.id
-      );
-      if (payment) {
-        const data = {
-          message: "payment done successfully",
-          amount: payment.amount,
-        };
-        return handleResponse(200, data, res);
-      }
+    const payment = await stripeService.confirmPayment(
+      paymentIntent.id,
+      default_source
+    );
+    if (payment) {
+      const data = {
+        message: "payment done successfully",
+        amount: payment.amount / 100,
+      };
+      return handleResponse(200, data, res);
     }
-  }
-  return handleError(500, res);
-});
-
-exports.getCard = catchAsync(async (req, res) => {
-  const { customerId } = req.body;
-  const { default_source } = await stripeController.getCustomer(customerId);
-  if (default_source) {
-    const card = await stripeController.getCard(customerId, default_source);
-    return handleResponse(200, card, res);
-  }
-  return handleError(500, res);
-});
-
-exports.deleteCard = catchAsync(async (req, res) => {
-  const { customerId } = req.body;
-  const { default_source } = await stripeController.getCustomer(customerId);
-  if (default_source) {
-    const card = await stripeController.deleteCard(customerId, default_source);
-    return handleResponse(200, card, res);
   }
   return handleError(500, res);
 });
